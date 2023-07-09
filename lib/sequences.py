8e844b55b86d905c3e8e0120824cd51e19d0cbce
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-07-09 19:58:21 -0400

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+       my_list=[]
+       first = 0
+       second = 1
+       tmp = 0
+       for i in range(length):
+        if i in {0,1}: 
+           my_list.append(i)
+        else:
+            my_list.append( first + second) 
+            tmp=second
+            second=first + second
+            first=tmp
+            
+       print(my_list)  
